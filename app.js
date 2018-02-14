16cfa2f681b6fd257e5c4c03604fecb71631a0a9
alberto.eyo.contractor <alberto.eyo.contractor@bbva.com>
2018-02-14 09:09:44 +0100

    feat(app.js)
    Calling the firecast.io API for Pontevedra
    
    Lesson 4, Section 34

--- app.js
+++ app.js
@@ -1,23 +1,41 @@
-const yargs = require('yargs');
-const geocode = require('./geocode/geocode');
+const request = require('request');
 
-const argv = yargs
-  .options({
-    a: {
-      demand: true,
-      alias: 'address',
-      describe: 'Address to fetch weather for',
-      string: true
-    }
-  })
-  .help()
-  .alias('help', 'h')
-  .argv;
+// const yargs = require('yargs');
+// const geocode = require('./geocode/geocode');
 
-geocode.geocodeAddress(argv.address, (errorMessage, results) => {
-  if (errorMessage) {
-    console.log(errorMessage);
+// const argv = yargs
+//   .options({
+//     a: {
+//       demand: true,
+//       alias: 'address',
+//       describe: 'Address to fetch weather for',
+//       string: true
+//     }
+//   })
+//   .help()
+//   .alias('help', 'h')
+//   .argv;
+
+// geocode.geocodeAddress(argv.address, (errorMessage, results) => {
+//   if (errorMessage) {
+//     console.log(errorMessage);
+//   } else {
+//     console.log(JSON.stringify(results, undefined, 2));
+//   }
+// });
+
+// 15eabf12900bdd1eb1e2bdbb9a7560c8
+// https://api.darksky.net/forecast/15eabf12900bdd1eb1e2bdbb9a7560c8/42.4298846,-8.6446202
+
+// body.currently.temperature
+
+request({
+  url: 'https://api.darksky.net/forecast/15eabf12900bdd1eb1e2bdbb9a7560c8/42.4298846,-8.6446202?lang=es&units=si',
+  json: true
+}, (error, response, body) => {
+  if (!error && response.statusCode === 200) {
+    console.log('Temperature in Pontevedra now', body.currently.temperature, 'ºC');
   } else {
-    console.log(JSON.stringify(results, undefined, 2));
+    console.log('Unable to fetch weather');
   }
 });
