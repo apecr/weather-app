d0e33c723b0d154f13b4482818dbf0f1e6d63fdb
alberto.eyo.contractor <alberto.eyo.contractor@bbva.com>
2018-02-14 08:32:25 +0100

    refactor()
    Refactor, include the request REST in other file
    
    Lesson 4, Sectoin 33

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
-const request = require('request');
 const yargs = require('yargs');
+const geocode = require('./geocode/geocode');
 
 const argv = yargs
   .options({
@@ -14,19 +14,4 @@ const argv = yargs
   .alias('help', 'h')
   .argv;
 
-request({
-  url: `https://maps.googleapis.com/maps/api/geocode/json?address=${encodeURIComponent(argv.address)}`,
-  json: true
-}, (error, response, body) => {
-  if (error) {
-    //
-    console.log('Unable to connect to Google Servers.');
-  } else if (body.status === 'ZERO_RESULTS') {
-    //
-    console.log('Unable to find the address', argv.address);
-  } else if (body.status === 'OK') {
-    console.log(`Address: ${body.results[0].formatted_address}`);
-    console.log(`Latitude: ${body.results[0].geometry.location.lat}`);
-    console.log(`Longitudd: ${body.results[0].geometry.location.lng}`);
-  }
-});
+geocode.geocodeAddress(argv.address);
